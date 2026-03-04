b667abcfbab449d54bd2516dd5a4afa4c3f74a3f
Anditi Imelda Jutta <imeldajutta@gmail.com>
2026-03-04 17:03:55 +0300

    Create Password_Generator.py

---
+++ Day2/Password_Generator.py
@@ -0,0 +1,27 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+chosen_letters = random.sample(letters,nr_letters)
+chosen_symbols = random.sample(symbols, nr_symbols)
+chosen_numbers = random.sample(numbers, nr_numbers)
+password = [chosen_letters, chosen_symbols, chosen_numbers]
+
+
+combined_password = []
+for mini_list in password:
+    for character in mini_list:
+        combined_password.append(character)
+print(combined_password)
+random.shuffle(combined_password)
+print(combined_password)
+
+one_line = "".join(combined_password)
+print(f"Your password is: {one_line}")
